9bf630070975353e32fc1188bfdcea229ed651dd
Manoj Kumar Jha <manoj.jha@zehntech.com>
2022-05-23 18:09:00 +0530

    Updated EC2 instance using Boto3.

--- python_with_aws/sourcecode/aws_ec2_create.py
+++ python_with_aws/sourcecode/aws_ec2_create.py
@@ -19,12 +19,28 @@ def create_aws_ec2():
 def check_instance():
     try:
         resource_ec2 = boto3.client("ec2")
-        instances = resource_ec2.describe_instances(Filters=[{'Name': 'instance-state-name', 'Values': ['running']}]).get("Reservations")
+        instances = resource_ec2.describe_instances().get("Reservations")
         for instance in instances:
             if "spot_instance_request_id" in instance:
                 print("instance is not on-demand")
             else:
-                response = resource_ec2.get_reservation_purchase_recommendation()
+                client = boto3.client('ce')
+                print("Reserved instance")
+                response = client.get_reservation_purchase_recommendation(
+                    AccountId='153543828477',
+                    Service='ec2',
+                    AccountScope='PAYER' or 'LINKED',
+                    LookbackPeriodInDays='SEVEN_DAYS' or 'THIRTY_DAYS' or 'SIXTY_DAYS',
+                    TermInYears='ONE_YEAR' or 'THREE_YEARS',
+                    PaymentOption='NO_UPFRONT' or 'PARTIAL_UPFRONT' or 'ALL_UPFRONT' or 'LIGHT_UTILIZATION' or 'MEDIUM_UTILIZATION' or 'HEAVY_UTILIZATION',
+                    ServiceSpecification={
+                        'EC2Specification': {
+                            'OfferingClass': 'STANDARD' or 'CONVERTIBLE'
+                        }
+                    },
+                    PageSize=123
+                    
+                )
     except Exception as e:
         print(e)
 create_aws_ec2()
